cedf395f796ecc062d6d1aa84d7cb79264e7d6d9
BenHall <ben@benhall.me.uk>
2010-03-14 14:05:20 +0000

    Changed to relative path

--- features/step_definitions/application.rb
+++ features/step_definitions/application.rb
@@ -1,6 +1,6 @@
 Given /^the application has started$/ do
   host = ApplicationLauncher.new
-  @app = host.launch 'D:/SourceControl/wipflash/ExampleUIs/bin/Debug/ExampleUIs.exe'
+  @app = host.launch Dir.pwd + '/src/ExampleUIs/bin/Debug/ExampleUIs.exe'
   @main_window = @app.find_window 'petShopWindow'
 end
 
